f1704d45d7d1e03838ea21c4f5df14bda214d90f
nakae-t <2301180@s.asojuku.ac.jp>
2024-11-26 10:36:59 +0900

    ナビゲーションメニュー

--- 5-1/script/script.js
+++ 5-1/script/script.js
@@ -1,8 +1,12 @@
-new Vue({
+  new Vue({
     el: '#app',
     data() {
       return {
-        
+        item: [
+          {id:1,label:"子"},
+          {id:2,label:"丑"},
+          {id:1,label:"寅"}
+        ]
       };
     }
   });
